d490930e1d0a8a230818c6c6ccd8c9ed08436558
Guillaume Witz <guiwitz@users.noreply.github.com>
2019-11-19 12:57:53 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='naparimovie',
+      version='0.1',
+      description='Generating movies from napari views',
+      url='https://github.com/guiwitz',
+      author='Guillaume Witz',
+      author_email='',
+      license='BSD3',
+      packages=['naparimovie'],
+      zip_safe=False,
+      install_requires=['pyquaternion'],
+      )
